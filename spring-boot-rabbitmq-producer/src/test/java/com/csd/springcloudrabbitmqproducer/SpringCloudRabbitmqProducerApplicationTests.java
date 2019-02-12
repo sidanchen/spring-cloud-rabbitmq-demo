e9feba5e92af54f19536d53a7edc267a9572fcf4
chensd <chensd@todofinance.com>
2019-02-12 14:29:50 +0800

    add 444

--- spring-boot-rabbitmq-producer/src/test/java/com/csd/springcloudrabbitmqproducer/SpringCloudRabbitmqProducerApplicationTests.java
+++ spring-boot-rabbitmq-producer/src/test/java/com/csd/springcloudrabbitmqproducer/SpringCloudRabbitmqProducerApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
-//添加注释1111333
+//添加注释1111333444
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringCloudRabbitmqProducerApplicationTests {

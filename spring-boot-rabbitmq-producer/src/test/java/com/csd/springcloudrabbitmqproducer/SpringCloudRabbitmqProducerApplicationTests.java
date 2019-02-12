1db04c8f1ec8021020e82e3bd0f486731a4f5d8e
chensd <chensd@todofinance.com>
2019-02-12 14:08:38 +0800

    add 1111

--- spring-boot-rabbitmq-producer/src/test/java/com/csd/springcloudrabbitmqproducer/SpringCloudRabbitmqProducerApplicationTests.java
+++ spring-boot-rabbitmq-producer/src/test/java/com/csd/springcloudrabbitmqproducer/SpringCloudRabbitmqProducerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//添加注释
+//添加注释1111
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringCloudRabbitmqProducerApplicationTests {

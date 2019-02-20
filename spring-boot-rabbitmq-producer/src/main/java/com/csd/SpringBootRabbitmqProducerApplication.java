4d6243d3e9782f247bb99d8ea9e351826c4235d8
Chensidan <1536242521@qq.com>
2019-02-20 09:46:55 +0800

    add 4444

--- spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
+++ spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
@@ -2,7 +2,7 @@ package com.csd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//111122223333
+//1111222233334444
 @SpringBootApplication
 public class SpringBootRabbitmqProducerApplication {
 

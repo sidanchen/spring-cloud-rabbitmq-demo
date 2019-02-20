db84ee0cf87eee038fa7995be4af80ba60cc9307
Chensidan <1536242521@qq.com>
2019-02-20 09:35:23 +0800

    add 3333

--- spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
+++ spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
@@ -2,7 +2,7 @@ package com.csd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//11112222
+//111122223333
 @SpringBootApplication
 public class SpringBootRabbitmqProducerApplication {
 

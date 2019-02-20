f235c8b574e7b4db5edc736604013c953b122c86
chensd <chensd@todofinance.com>
2019-02-20 09:49:29 +0800

    add 5555

--- spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
+++ spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
@@ -2,7 +2,7 @@ package com.csd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//111122223333
+//1111222233335555
 @SpringBootApplication
 public class SpringBootRabbitmqProducerApplication {
 

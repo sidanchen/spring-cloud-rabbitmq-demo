7005c9b26db82b7590be4c60dadd4bd70720c2b0
chensd <chensd@todofinance.com>
2019-02-22 09:03:55 +0800

    add 9999

--- spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
+++ spring-boot-rabbitmq-producer/src/main/java/com/csd/SpringBootRabbitmqProducerApplication.java
@@ -2,7 +2,7 @@ package com.csd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//11112222333344445555666677778888
+//111122223333444455556666777788889999
 @SpringBootApplication
 public class SpringBootRabbitmqProducerApplication {
 
